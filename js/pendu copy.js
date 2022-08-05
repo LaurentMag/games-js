7a19b35c00759b2c352722dee259fec6e577d808
Laurent <magrez0laurent@gmail.com>
2022-08-05 15:30:41 +0200

    removed input ( kept backup files ) to have global event input listener

--- js/pendu copy.js
+++ js/pendu copy.js
@@ -1,48 +1,42 @@
 "use strict";
 
+const form = document.querySelector(".form-pendu");
+const input = document.querySelector("#penduLetterId");
 const penduWord = document.querySelector(".pendu-result");
-const displaySelectedLetter = document.querySelector(".pendu-displayLetter");
+
 const body = document.querySelector("body");
+
+body.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const regex = /[A-Za-z]/;
+
+  // check if the pressed key is a letter and
+  // only length of 1 ( otherwise can display "shift" / "command"... and other function keys)
+  if (e.key.length === 1 && e.key.match(regex) !== null) {
+    // display the letter in the input field
+    // e.target.value = e.key;
+
+    console.log(e.key);
+  }
+  // if (e.key == "Backspace") {
+  //   // letter can be "deleted"
+  //   e.target.value = ""; // clear the display
+  // }
+});
 
 const PENDU_SETTING = {
   oneOrTwoPlayer: 1,
   newGame: true,
   lifeCount: 6,
-  //
-  selectedLetter: "",
-  // fetched txt as array
-  txtToArray: [],
-  //
-  newGameWord: "",
-  hiddenWord: [],
 };
 
-/*
+let txtToArray = [];
+let newGameWord = "";
+let hiddenWord = [];
 
-
-
-*/
-
-const letterContainer = document.querySelector(".pendu-displayLetter-container");
-
-const test = document.querySelector(".hide-test");
-
-test.addEventListener("click", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-
-  console.log("click");
-
-  letterContainer.classList.toggle("hiddenLetter");
-});
-
-/*
-
-
-
-*/
-
-// =====================================================================================================================
+// ==================================================================
 // UTILITIES
 // get a random index from an array
 const randomIndex = (array) => {
@@ -51,16 +45,12 @@ const randomIndex = (array) => {
 
 const hideWord = (wordAsArray) => {
   for (let i = 0; i < wordAsArray.length; i++) {
-    PENDU_SETTING.hiddenWord.push("-");
+    hiddenWord.push("-");
   }
 };
 
-const sendHiddenWordToHtml = (toDisplay) => {
+const sendResultToHtml = (toDisplay) => {
   penduWord.innerHTML = `${toDisplay.join(" ")}`;
-};
-
-const sendSelectLetterToHtml = (letter) => {
-  displaySelectedLetter.innerHTML = `${letter}`;
 };
 
 // =====================================================================================================================
@@ -69,7 +59,7 @@ const toArray = (textParam) => {
   // set text into array
   // use regex to "split" with space (\s) coma (,) or next line (\n)
   // + mean : eventualy several
-  return (PENDU_SETTING.txtToArray = textParam.split(/[\s,\n]+/));
+  return (txtToArray = textParam.split(/[\s,\n]+/));
 };
 
 // FETCH .TXT ---------------------
@@ -85,71 +75,70 @@ const fetchPenduTxt = () => {
     })
     // then trigger the getWord
     // as its async function can be declared after ( as const )
-    .then(() => getWord(PENDU_SETTING.txtToArray));
+    .then(() => getWord(txtToArray));
 };
 
 // get a random new word when new game
 // setup 1player
 const getWord = (array) => {
-  // reset the PENDU_SETTING.hiddenWord array at each "start / restart"
-  PENDU_SETTING.hiddenWord = [];
+  // reset the hiddenWord array at each "start / restart"
+  hiddenWord = [];
   if (PENDU_SETTING.newGame === true) {
     // set PENDU_SETTING.newGame to false to prevent get a new word as long as preview game isnt finished
     PENDU_SETTING.newGame = false;
 
     if (PENDU_SETTING.oneOrTwoPlayer == 1) {
       // select a random word from the .txt turned as array
-      PENDU_SETTING.newGameWord = array[randomIndex(array)];
+      newGameWord = array[randomIndex(array)];
       // create the hidden word to be displayed in the html
-      hideWord(PENDU_SETTING.newGameWord);
+      hideWord(newGameWord);
       // set the html display
-      sendHiddenWordToHtml(PENDU_SETTING.hiddenWord);
+      sendResultToHtml(hiddenWord);
     }
   }
 };
 
 // =====================================================================================================================
 
-// check if "letter" belong to the PENDU_SETTING.newGameWord
+// check if "letter" belong to the newGameWord
 const checkForLetter = (letter) => {
-  for (const letterIndex in PENDU_SETTING.newGameWord) {
-    if (letter.toLowerCase() === PENDU_SETTING.newGameWord.toLowerCase().charAt(letterIndex)) {
+  for (const letterIndex in newGameWord) {
+    if (letter.toLowerCase() === newGameWord.toLowerCase().charAt(letterIndex)) {
       //
       console.log("trouvé", letter, "at index : ", letterIndex);
       //
       if (letterIndex === 0) {
         // set first letter un uppercase
-        PENDU_SETTING.hiddenWord[letterIndex] = letter.toUpperCase();
-        sendHiddenWordToHtml(PENDU_SETTING.hiddenWord);
+        hiddenWord[letterIndex] = letter.toUpperCase();
+        sendResultToHtml(hiddenWord);
       } else {
-        PENDU_SETTING.hiddenWord[letterIndex] = letter.toLowerCase();
-        sendHiddenWordToHtml(PENDU_SETTING.hiddenWord);
+        hiddenWord[letterIndex] = letter.toLowerCase();
+        sendResultToHtml(hiddenWord);
       }
     }
   }
 };
 
 const winConsCheck = () => {
-  if (PENDU_SETTING.hiddenWord.includes("-") === true) {
+  if (hiddenWord.includes("-") === true) {
     PENDU_SETTING.lifeCount -= 1;
 
     if (PENDU_SETTING.lifeCount === 0) {
       console.log("PERDU");
       PENDU_SETTING.newGame = true;
-      getWord(PENDU_SETTING.txtToArray);
+      getWord(txtToArray);
     }
   } else {
     // no more "-" mean word found,
     // set newGame to true to restart the "getword"
     console.log(" VICTORY ");
     PENDU_SETTING.newGame = true;
-    getWord(PENDU_SETTING.txtToArray);
+    getWord(txtToArray);
   }
 };
 
 const hangmanLogic = (letterFromInput) => {
-  console.log("current word : ", PENDU_SETTING.newGameWord);
-  console.log(letterFromInput);
+  console.log("current word : ", newGameWord);
   // check if the letter is in the selected word
   checkForLetter(letterFromInput);
   // at each letter input check for the winCons / lose
@@ -157,8 +146,20 @@ const hangmanLogic = (letterFromInput) => {
 };
 
 // =====================================================================================================================
+// ON KEYDOWN EVENT ---------------------
+const preventDefaultInputDisplay = (e) => {
+  // need to exclude "Enter" to be able to submit the form with it
+  if (e.key !== "Enter") {
+    // as its key down ( come before key up ), would let special character to display in the input.
+    // the preventDefaut() will make the input key down behavior to be totaly ignored
+    e.preventDefault();
+    e.stopPropagation();
+  }
+};
 
-body.addEventListener("keyup", (e) => {
+// ON KEYUP EVENT ---------------------
+// force input to display the pressed key and only "A" to "Z" character
+const inputFilterForDisplay = (e) => {
   e.preventDefault();
   e.stopPropagation();
 
@@ -167,29 +168,40 @@ body.addEventListener("keyup", (e) => {
   // check if the pressed key is a letter and
   // only length of 1 ( otherwise can display "shift" / "command"... and other function keys)
   if (e.key.length === 1 && e.key.match(regex) !== null) {
-    PENDU_SETTING.selectedLetter = e.key;
-    sendSelectLetterToHtml(PENDU_SETTING.selectedLetter);
+    // display the letter in the input field
+    e.target.value = e.key;
   }
-
-  // SEND TO HANGMANLOGIC THE LETTER IF ENTER & IF LETTER ISNT <empty.string>
-  if (e.key === "Enter" && PENDU_SETTING.selectedLetter !== "") {
-    hangmanLogic(PENDU_SETTING.selectedLetter);
-    PENDU_SETTING.selectedLetter = "";
-    sendSelectLetterToHtml(PENDU_SETTING.selectedLetter);
-
-    // SEND ERR MESS IF ENTER & LETTER IS <empty.string>
-    // ( else if as need to check if can send first, otherwise will send err mess after each succesfull send)
-  } else if (e.key === "Enter" && PENDU_SETTING.selectedLetter === "") {
-    console.log("Type a Key");
-  }
-
-  // DELETED LETTER
   if (e.key == "Backspace") {
-    PENDU_SETTING.selectedLetter = "";
-    sendSelectLetterToHtml(PENDU_SETTING.selectedLetter);
+    // letter can be "deleted"
+    e.target.value = ""; // clear the display
   }
-});
+};
+
+// FORM submit : get input key  ---------------------
+const formManagement = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (e.target.penduLetterId.value !== "") {
+    // trigger game logic with each letter submit
+    hangmanLogic(e.target.penduLetterId.value);
+    // clear input field after form submit
+    setTimeout(() => {
+      e.target.penduLetterId.value = "";
+    }, 250);
+    // if its not a letter
+  } else {
+    console.log("enter a letter");
+  }
+};
 
 // =====================================================================================================================
 // trigger fetch at start ( this will trigger the getWord for the first game)
 fetchPenduTxt();
+// generate listener :
+// manage what to display in the input
+input.addEventListener("keyup", inputFilterForDisplay);
+// prevent any input display from typing
+input.addEventListener("keydown", preventDefaultInputDisplay);
+// for submit management
+form.addEventListener("submit", formManagement);
